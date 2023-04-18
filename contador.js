22bb77e5f1b7f953b2e424cc99ab1d6e86c3d29d
Angel Corral <82479794+angelcorral2021@users.noreply.github.com>
2023-04-17 21:17:45 -0400

    Web el Faro

---
+++ contador.js
@@ -0,0 +1,15 @@
+
+/*let cantidadNoticias = document.querySelectorAll('.noticia').length;
+const miElemento = document.getElementById('contador');
+miElemento.innerHTML = cantidadNoticias;*/
+
+
+
+function actualizarContador() {
+    let cantidadNoticias = document.querySelectorAll('.noticia').length;
+    const miElemento = document.getElementById('contador');
+    miElemento.innerHTML = cantidadNoticias;
+}
+
+
+setInterval(actualizarContador, 1000);
